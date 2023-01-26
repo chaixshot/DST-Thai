44d12870e4a111e0b485ca1417ea034772d9c79c
H@mer <13221018+chaixshot@users.noreply.github.com>
2023-01-26 15:04:51 +0700

    Add files via upload

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 name = " ภาษาไทย"
-version = "5.53"
+version = "5.55"
 author = "H@mer"
 description = [[Don't Starve Together:ภาษาไทย
 เวอร์ชั่น: ]]..version..
@@ -14,7 +14,7 @@ description = [[Don't Starve Together:ภาษาไทย
 หรือข้อผิดพลาดอื่นๆกรุณาแจ้งผู้พัฒนาด้วย เพื่อจะได้ทำการแก้ไขปรับปรุงในการอัพเดท
 สามารถพูดคุยหรือติดต่อสอบถามและเป็นกำลังใจได้ที่โพสบน Workshop
 
-DST รุ่น.(v527664)
+DST รุ่น.(v540053)
 ]]
 forumthread = ""
 api_version = 10
