dc8781ec6482723f52c49463a265abf51b058c3e
chaixshot <chai5142xshot@gmail.com>
2025-07-26 21:44:43 +0700

    chamge: Add tool conItem for print lowercase item name in conversion

--- scripts/conItem/conItem.lua
+++ scripts/conItem/conItem.lua
@@ -4,70 +4,13 @@ if not Config.CON_ITEM or (not Config.ITEM and not Config.ITEM_TWO) then
 end
 
 -- Anti duplicate names (STRINGS.NAMES.TICOON)
-local itemsName = {}
+local itemsName = {
+   ["beefalo bell"] = Thai.PO["STRINGS.NAMES.BEEF_BELL"],
+   ["beefalo"] = Thai.PO["STRINGS.NAMES.BEEFALO"],
+}
 for nameIndex, nameEng in pairs(GetOriginalStringsFromIndex("STRINGS.NAMES")) do
    itemsName[nameEng:gsub("?", "")] = Thai.PO[nameIndex]
 end
-
--- INFO: Tool to generate conItemIndex
---[[ local conStrings = {"STRINGS.CHARACTERS"}
-if Config.UI then
-   table.insert(conStrings, "STRINGS.RECIPE_DESC")
-   if IsDST then
-      table.insert(conStrings, "STRINGS.SKILLTREE")
-      table.insert(conStrings, "STRINGS.SKIN_DESCRIPTIONS")
-   end
-end
-
-local missingName = {}
-for _, text in ipairs(conStrings) do
-   local blackList = {["Nothing"] = true, ["X"] = true, ["Health"] = true, ["Sanity"] = true, ["Fire"] = true, ["Plant"] = true}
-   for conIndex, conEng in pairs(GetOriginalStringsFromIndex(text)) do
-      local conThai = Thai.PO[conIndex]
-      local found = {}
-      local countEng = {}
-      local countThai = {}
-
-      if conThai then
-         -- conEng = conEng:lower()
-         -- conThai = conThai:lower()
-         for nameEng, nameThai in pairs(itemsName) do
-            -- nameEng = nameEng:lower()
-            if not blackList[nameEng] then
-               if conEng:find(nameEng) then -- Fast check
-                  if conEng:find("%f[%a]"..nameEng.."%f[%A]") then -- Slow check
-                     conThai = conThai:gsub("%f[%a]"..nameEng.."%f[%A]", nameThai)
-                     table.insert(countEng, nameEng)
-                     table.insert(found, nameEng)
-                  end
-               end
-            end
-            if conThai:find(nameThai) then -- Fast check
-               table.insert(countThai, nameThai)
-            end
-         end
-
-         if #found > 0 then
-            local index = ""
-            for k, v in pairs(found) do
-               index = index.."[\""..v.."\"] = true, "
-            end
-            print("[\""..conIndex.."\"] = {"..index.."},")
-         end
-
-         if #countEng > #countThai then
-            missingName[conIndex] = countEng
-         end
-      end
-   end
-end
-for k, v in pairs(missingName) do
-   local missing = ""
-   for i, j in pairs(v) do
-      missing = missing.."\""..j.."\", "
-   end
-   print("\""..k.."\"\t\t"..missing) -- print missing thai name from converation
-end ]]
 
 local conItemIndex = require("conItem/conItemIndex")
 for conIndex, checkList in pairs(conItemIndex) do
@@ -89,8 +32,77 @@ for conIndex, checkList in pairs(conItemIndex) do
             conThai = conThai:gsub(nameThai, nameThai.."("..nameEng..")")
          end
 
-         conThai = conThai:gsub("  ", " ")
-         Thai.PO[conIndex] = conThai
+         Thai.PO[conIndex] = conThai:gsub("  ", " ")
       end
    end
 end
+
+
+-- INFO: Tool to generate conItemIndex
+--[[ local conStrings = {"STRINGS.CHARACTERS"}
+if Config.UI then
+   table.insert(conStrings, "STRINGS.RECIPE_DESC")
+   if IsDST then
+      table.insert(conStrings, "STRINGS.SKILLTREE")
+      table.insert(conStrings, "STRINGS.SKIN_DESCRIPTIONS")
+   end
+end
+
+local missingName = {}
+for _, text in ipairs(conStrings) do
+   local blackList = {["Nothing"] = true, ["X"] = true, ["Health"] = true, ["Sanity"] = true, ["Fire"] = true, ["Plant"] = true}
+   for conIndex, conEng in pairs(GetOriginalStringsFromIndex(text)) do
+      local conThai = Thai.PO[conIndex]
+      local found = {}
+      local foundLower = {}
+      local countEng = {}
+      local countThai = {}
+
+      if conThai then
+         for nameEng, nameThai in pairs(itemsName) do
+            if not blackList[nameEng] then
+               if conEng:find(nameEng) then -- Fast check
+                  if conEng:find("%f[%a]"..nameEng.."%f[%A]") then -- Slow check
+                     conThai = conThai:gsub("%f[%a]"..nameEng.."%f[%A]", nameThai)
+                     table.insert(countEng, nameEng)
+                     table.insert(found, nameEng)
+                  end
+               elseif conEng:find("%f[%a]"..nameEng:lower().."%f[%A]") and not conEng:find("_") and not blackList[nameEng:lower()] then
+                  table.insert(foundLower, nameEng:lower())
+               end
+            end
+            if conThai:find(nameThai) then -- Fast check
+               table.insert(countThai, nameThai)
+            end
+         end
+
+         -- if #found > 0 then
+         --    local index = ""
+         --    for k, v in pairs(found) do
+         --       index = index.."[\""..v.."\"] = true, "
+         --    end
+         --    print("[\""..conIndex.."\"] = {"..index.."},")
+         -- end
+
+         -- print lowercase item name in conversion
+         -- if #foundLower > 0 then
+         --    local index = ""
+         --    for k, v in pairs(foundLower) do
+         --       index = index.."\""..v.."\", "
+         --    end
+         --    print(index)
+         -- end
+
+         if #countEng > #countThai then
+            missingName[conIndex] = countEng
+         end
+      end
+   end
+end ]]
+--[[ for k, v in pairs(missingName) do -- print missing thai name from converation
+   local missing = ""
+   for i, j in pairs(v) do
+      missing = missing.."\""..j.."\", "
+   end
+   print("\""..k.."\"\t\t"..missing)
+end ]]
